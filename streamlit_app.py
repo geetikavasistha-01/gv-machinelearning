5bd77a2f6dc8a74585379190c5760f05c29c14db
geetikavasistha-01 <geetikavasistha13@gmail.com>
2025-04-15 12:33:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine learning App')
 
 st.write('This app builds a machine learning model!')
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df= pd.read_csv('https://raw.githubusercontent.com/geetikavasistha-01/gv-machinelearning/refs/heads/master/penguins.csv')
+  df
+
+
+  st.write('**X**')
+  X= df.drop('species'. axis=1)
+  X
+
+  st.write('**y**')
+  y=df.species
+  y
